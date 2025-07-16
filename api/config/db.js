761c2b136d7da3616955843ba5c95af5e9b86ef6
Honey-Joe <honeyjoe943@gmail.com>
2025-07-16 16:58:37 +0530

    fix: add connection options for mongoose to improve database connection handling

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    });
     console.log("Mongo DB Connected");
   } catch (error) {
     console.log("Mongo DB Not Connected ", error);
