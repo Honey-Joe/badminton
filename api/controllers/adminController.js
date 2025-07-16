7da502d153c3240f231559880f26a326f9106746
Honey-Joe <honeyjoe943@gmail.com>
2025-07-16 16:52:32 +0530

    fix: remove secure flag from cookie options in admin and auth controllers; update API_URL to use localhost

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -96,7 +96,6 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: false,
-    secure: process.env.NODE_ENV === 'production'
   };
   
   res.cookie('jwt_admin', token, cookieOptions);
--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -16,7 +16,6 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: false,
-    secure: process.env.NODE_ENV === 'production'
   };
   
   res.cookie('jwt', token, cookieOptions);
